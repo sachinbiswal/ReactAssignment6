62034679d8bb155fddfe7b379ff0edbb55cbc148
sachin kumar biswal <sachinkbiswal@gmail.com>
2023-05-31 18:01:49 +0530

    React Assignment6

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+import Navbar from './Component/Navbar';
+import Main from './Component/Main';
+import Footer from './Component/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{width : "1000px",height  :"600px",border : "none",marginLeft : '10%',display  : "flex",justifyContent : 'center',flexDirection : 'column',alignItems : "center",boxShadow  : "0 0 20px black"}}>
+      <Navbar />
+      <Main />
+      <Footer />
+
     </div>
   );
 }
